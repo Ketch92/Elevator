b8caba63a032c9e67c6d1ac6c6cfefda40a296d6
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-22 22:37:01 +0200

    Added test class for Building class

---
+++ src/test/java/building/BuildingTest.java
@@ -0,0 +1,9 @@
+package building;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildingTest {
+
+    private Building building = new Building.BuildingConstructor().build();
+    
+}
