face12826012676608e6dc5303988448a3a304cd
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-22 21:07:50 +0200

    Implemented tests for Floor class

--- src/main/java/building/Floor.java
+++ src/main/java/building/Floor.java
@@ -3,37 +3,34 @@ package building;
 import java.util.List;
 
 public class Floor {
-    private String name;
-    private int level;
-    private List<Integer> queue;
+  private String name;
+  private int level;
+  private List<Integer> queue;
 
-    public Floor () {
+  public Floor(int level, List<Integer> queue) {
+    this.queue = queue;
+    this.level = level;
+    name = "Level " + level;
+  }
 
-    }
+  public String getName() {
+    return name;
+  }
 
-    public Floor(int level, List<Integer> queue) {
-        this.queue = queue;
-        this.level = level;
-        name = "Level " + level;
-    }
+  public int getLevel() {
+    return level;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setLevel(int level) {
+    this.level = level;
+    name = "Level " + level;
+  }
 
-    public int getLevel() {
-        return level;
-    }
+  public List<Integer> getQueue() {
+    return queue;
+  }
 
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public List<Integer> getQueue() {
-        return queue;
-    }
-
-    public void setQueue(List<Integer> queue) {
-        this.queue = queue;
-    }
+  public void setQueue(List<Integer> queue) {
+    this.queue = queue;
+  }
 }
--- src/test/java/building/FloorTest.java
+++ src/test/java/building/FloorTest.java
@@ -1,36 +1,42 @@
 package building;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class FloorTest {
-
-    private Floor floor;
-
-    @BeforeEach
-    void setUp() {
-        floor = new Floor();
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void getLevel() {
-    }
-
-    @Test
-    void setLevel() {
-    }
-
-    @Test
-    void getQueue() {
-    }
-
-    @Test
-    void setQueue() {
-    }
+  private final String expectedName = "Level 5";
+  private final int expectedLevel = 5;
+  private final List<Integer> expectedQueue = List.of(5, 4, 3, 2, 1, 0);
+  private final Floor floor = new Floor(expectedLevel, expectedQueue);
+  
+  @Test
+  void getName() {
+    Assertions.assertEquals(expectedName, floor.getName());
+  }
+  
+  @Test
+  void getLevel() {
+    Assertions.assertEquals(expectedLevel, floor.getLevel());
+  }
+  
+  @Test
+  void setLevel() {
+    int newLevel = 10;
+    floor.setLevel(newLevel);
+    Assertions.assertEquals(newLevel, floor.getLevel());
+    Assertions.assertEquals("Level " + newLevel, floor.getName());
+  }
+  
+  @Test
+  void getQueue() {
+    Assertions.assertEquals(expectedQueue, floor.getQueue());
+  }
+  
+  @Test
+  void setQueue() {
+    List<Integer> newQueue = List.of(4, 4, 3, 10, 2, 4);
+    floor.setQueue(newQueue);
+    Assertions.assertEquals(newQueue, floor.getQueue());
+  }
 }
