d6d9796cad6a989e56fb412852592c10ee424224
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-22 21:30:55 +0200

    added tests to methods pushToQueue and popFromQueue to Floor class

--- src/test/java/building/FloorTest.java
+++ src/test/java/building/FloorTest.java
@@ -1,14 +1,22 @@
 package building;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class FloorTest {
-  private final String expectedName = "Level 5";
-  private final int expectedLevel = 5;
-  private final List<Integer> expectedQueue = List.of(5, 4, 3, 2, 1, 0);
-  private final Floor floor = new Floor(expectedLevel, expectedQueue);
+  private String expectedName = "Level 5";
+  private int expectedLevel = 5;
+  private List<Integer> expectedQueue = new ArrayList<>(List.of(5, 4, 3, 2, 1, 0));
+  private Floor floor = new Floor(expectedLevel, expectedQueue);
+  
+  @BeforeEach
+  void setUp() {
+    floor = new Floor(expectedLevel, expectedQueue);
+  }
   
   @Test
   void getName() {
@@ -38,5 +46,44 @@ class FloorTest {
     List<Integer> newQueue = List.of(4, 4, 3, 10, 2, 4);
     floor.setQueue(newQueue);
     Assertions.assertEquals(newQueue, floor.getQueue());
+  }
+  
+  @Test
+  void pushToQueue() {
+    int[] persons = new int[] {7, 8, 9};
+    int i = 0;
+    while (i < persons.length) {
+      floor.pushToQueue(persons[i]);
+      i++;
+    }
+    for (int person : persons) {
+      if(!floor.getQueue().contains(person)) {
+        Assertions.fail("Persons added to the queue weren't found there");
+      }
+    }
+  }
+  
+  @Test
+  void pushNull() {
+    Assertions.assertFalse(floor.pushToQueue(null));
+  }
+  
+  @Test
+  void popFromQueue() {
+    int size = expectedQueue.size();
+    for (int i = 0; i < size; i++) {
+      floor.popFromQueue(i);
+    }
+    Assertions.assertTrue(floor.getQueue().isEmpty());
+  }
+  
+  @Test
+  void popNull() {
+    Assertions.assertFalse(floor.popFromQueue(null));
+  }
+  
+  @Test
+  void popNonExistingPerson() {
+    Assertions.assertFalse(floor.popFromQueue(1000));
   }
 }
